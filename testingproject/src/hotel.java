fb400deca1983385e43528f90af4d206e517a149
redfly8 <redfly8@outlook.com>
2023-05-24 17:57:55 +0200

    d
    d

--- testingproject/src/hotel.java
+++ testingproject/src/hotel.java
@@ -1,45 +1,48 @@
 public class hotel {
-int stockwerke;
-int zimmerprostockwerk;
-int belegung;
-String name;
+    int stockwerke;
+    int zimmerprostockwerk;
+    int belegung;
+    String name;
 
-public int getFreieZimmer(){
-    int retvalue = stockwerke*zimmerprostockwerk - belegung;
-    return retvalue;
+    public int getFreieZimmer() {
+        int retvalue = stockwerke * zimmerprostockwerk - belegung;
+        return retvalue;
 
-}
-public boolean einchecken(){
-    if(belegung < stockwerke*zimmerprostockwerk){
-        belegung = belegung + 1;
-        return true;
-    }else{
-        return false;
     }
-    
-}
-public boolean auschecken(){
-    if(belegung < 1){
-        return false;
-    }else{
-        belegung = belegung - 1;
-        return true;
+
+    public boolean einchecken() {
+        if (belegung < stockwerke * zimmerprostockwerk) {
+            belegung = belegung + 1;
+            return true;
+        } else {
+            return false;
+        }
+
     }
-    
-}
-public void printinfo(boolean namebln, boolean zimmer, boolean stockwerkbln, boolean zimmerprostockwerkbln){
-    if(namebln == true){
-        System.out.println(name);
+
+    public boolean auschecken() {
+        if (belegung < 1) {
+            return false;
+        } else {
+            belegung = belegung - 1;
+            return true;
+        }
+
     }
-    if(zimmer == true){
-        System.out.println("Anzahl freie Zimmer:" +getFreieZimmer());
+
+    public void printinfo(boolean namebln, boolean zimmer, boolean stockwerkbln, boolean zimmerprostockwerkbln) {
+        if (namebln == true) {
+            System.out.println(name);
+        }
+        if (zimmer == true) {
+            System.out.println("Anzahl freie Zimmer:" + getFreieZimmer());
+        }
+        if (stockwerkbln == true) {
+            System.out.println("Anzahl Stockwerke:" + stockwerke);
+        }
+        if (zimmerprostockwerkbln == true) {
+            System.out.println("Anzahl Zimmer pro Stockwerk:" + zimmerprostockwerk);
+        }
     }
-    if(stockwerkbln == true){
-        System.out.println("Anzahl Stockwerke:" +stockwerke);
-    }
-    if(zimmerprostockwerkbln == true){
-        System.out.println("Anzahl Zimmer pro Stockwerk:" + zimmerprostockwerk);
-    }
-}
-   
+
 }
--- testingproject/src/mainhotell.java
+++ testingproject/src/mainhotell.java
@@ -5,10 +5,10 @@ public class mainhotell {
       edelweiss.belegung = 5;
       edelweiss.stockwerke = 3;
       edelweiss.name = "Hotel Edelweiss";
-      
+
       hotel astoria = new hotel();
       astoria.zimmerprostockwerk = 5;
-      astoria. belegung = 0;
+      astoria.belegung = 0;
       astoria.stockwerke = 2;
       astoria.name = "Hotel Astoria";
 
@@ -18,30 +18,30 @@ public class mainhotell {
       alpenblick.stockwerke = 6;
       alpenblick.name = "Hotel Alpenblick";
 
-      if(astoria.einchecken() == false){
+      if (astoria.einchecken() == false) {
          System.out.println("Du hast versucht, einzuchecken, es hat aber leider keinen Platz mehr.");
       }
-      if(astoria.auschecken() == false){
+      if (astoria.auschecken() == false) {
          System.out.println("Fehler: Du hast versucht auszuchecken, du warst aber nie registriert.");
       }
       System.out.println(
-         ferieninfo(alpenblick) + 
-         ferieninfo(edelweiss) + 
-         ferieninfo(astoria));
-      
-    }
-    
-    
-    private static String ferieninfo(hotel hotelname){
-      int capacity = hotelname.stockwerke*hotelname.zimmerprostockwerk;
-      if(capacity - hotelname.belegung > 0){
-         String output = "\n"+ hotelname.name + "\n \taktuell " + hotelname.belegung + " von " + capacity + " Zimmern belegt.";
+            ferieninfo(alpenblick) +
+                  ferieninfo(edelweiss) +
+                  ferieninfo(astoria));
+
+   }
+
+   private static String ferieninfo(hotel hotelname) {
+      int capacity = hotelname.stockwerke * hotelname.zimmerprostockwerk;
+      if (capacity - hotelname.belegung > 0) {
+         String output = "\n" + hotelname.name + "\n \taktuell " + hotelname.belegung + " von " + capacity
+               + " Zimmern belegt.";
          return output;
-      }else{
-         String output = "\n"+hotelname.name + "\n"+"\tBesetzt ("+ hotelname.belegung + " von " +capacity+ " Zimmern belegt)";
+      } else {
+         String output = "\n" + hotelname.name + "\n" + "\tBesetzt (" + hotelname.belegung + " von " + capacity
+               + " Zimmern belegt)";
          return output;
       }
-      
 
-    }
    }
+}
